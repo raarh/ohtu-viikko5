4828de73b32b5929f6c3990d83f0c19e48ba6258
Arto Rantanen <arto.rantanen@helsinki.fi>
2016-04-15 13:16:01 +0300

    3.3

--- Verkkokauppa1/src/test/java/ohtu/VerkkokauppaTest.java
+++ Verkkokauppa1/src/test/java/ohtu/VerkkokauppaTest.java
@@ -85,4 +85,28 @@ public class VerkkokauppaTest {
         //  varmistettava että kutsutaan pankin metodia tilisiirto oikealla asiakkaalla,tilinumerolla ja summalla
         verify(pankki).tilisiirto(eq("pekka"),eq(42),eq("12345"),anyString(),eq(10));
     }
+    @Test
+    public void asioitiTesti3(){
+        // luodaan ensin mock-oliot
+        Pankki pankki = mock(Pankki.class);
+
+        Viitegeneraattori viite = mock(Viitegeneraattori.class);
+        // määritellään että viitegeneraattori palauttaa viitten 42
+        when(viite.uusi()).thenReturn(42);
+
+        Varasto varasto = mock(Varasto.class);
+        // määritellään että tuote numero 1 on maito jonka hinta on 5 ja saldo 1
+        when(varasto.saldo(1)).thenReturn(10);
+        when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
+        // sitten testattava kauppa
+        Kauppa k = new Kauppa(varasto, pankki, viite);
+        // tehdään ostokset
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1);     // ostetaan tuotetta numero 1 eli maitoa
+        k.lisaaKoriin(1);
+        k.tilimaksu("pekka", "12345");
+        //  varmistettava että kutsutaan pankin metodia tilisiirto oikealla asiakkaalla,tilinumerolla ja summalla
+        verify(pankki).tilisiirto(eq("pekka"),eq(42),eq("12345"),anyString(),eq(10));
+    }
+
 }
