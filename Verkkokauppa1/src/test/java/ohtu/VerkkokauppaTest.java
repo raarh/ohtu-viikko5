74881cfcb8374af4153d6343aa6de18aa8df2751
Arto Rantanen <arto.rantanen@helsinki.fi>
2016-04-15 13:32:21 +0300

    3.6

--- Verkkokauppa1/src/test/java/ohtu/VerkkokauppaTest.java
+++ Verkkokauppa1/src/test/java/ohtu/VerkkokauppaTest.java
@@ -137,7 +137,44 @@ public class VerkkokauppaTest {
         //  varmistettava että kutsutaan pankin metodia tilisiirto oikealla asiakkaalla,tilinumerolla ja summalla
         verify(pankki).tilisiirto(eq("pekka"), eq(42), eq("12345"), anyString(), eq(5));
     }
+    @Test
     public void asioitiTesti5(){
+        // luodaan ensin mock-oliot
+        Pankki pankki = mock(Pankki.class);
+
+        Viitegeneraattori viite = mock(Viitegeneraattori.class);
+        // määritellään että viitegeneraattori palauttaa viitten 42
+        when(viite.uusi()).thenReturn(42);
+        Varasto varasto = mock(Varasto.class);
+        // määritellään että tuote numero 1 on maito jonka hinta on 5 ja saldo 1
+        when(varasto.saldo(1)).thenReturn(10);
+        when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
+        when(varasto.haeTuote(2)).thenReturn(new Tuote(2, "Fink Bräu I", 2));
+        when(varasto.saldo(2)).thenReturn(1);
+        when(varasto.haeTuote(3)).thenReturn(new Tuote(3, "Porter", 11));
+        when(varasto.saldo(3)).thenReturn(1);
+        // sitten testattava kauppa
+        Kauppa k = new Kauppa(varasto, pankki, viite);
+        // tehdään ostokset
+        k.aloitaAsiointi();
+
+        k.lisaaKoriin(1);     // ostetaan tuotetta numero 1 eli maitoa
+
+        k.tilimaksu("pekka", "12345");
+        //  varmistettava että kutsutaan pankin metodia tilisiirto oikealla asiakkaalla,tilinumerolla ja summalla
+        verify(pankki).tilisiirto(eq("pekka"),eq(42),eq("12345"),anyString(),eq(5));
+
+        k.aloitaAsiointi();
+        k.lisaaKoriin(2);
+        k.tilimaksu("mas", "12345");
+        verify(pankki).tilisiirto(eq("mas"),eq(42),eq("12345"),anyString(),eq(2));
+        k.aloitaAsiointi();
+        k.lisaaKoriin(3);
+        k.tilimaksu("massi", "123465");
+        verify(pankki).tilisiirto(eq("massi"),eq(42),eq("123465"),anyString(),eq(11));
+    }
+    @Test
+    public void asioitiTesti6(){
         // luodaan ensin mock-oliot
         Pankki pankki = mock(Pankki.class);
 
